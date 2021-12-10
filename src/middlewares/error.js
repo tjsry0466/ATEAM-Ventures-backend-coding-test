b90b164480e2249246cb95e704aaf053483d9c17
jeong seonkyo <seonkyo0466@gmail.com>
2021-12-10 15:49:57 +0900

    env관련 설정 제거

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,37 +1,27 @@
 const httpStatus = require('http-status');
-const config = require('../config/');
-const logger = require('../config/logger');
 const ApiError = require('../utils/ApiError');
 
+// 에러를 ApiError객체로 변환
 const errorConverter = (err, req, res, next) => {
   let error = err;
   if (!(error instanceof ApiError)) {
-    const statusCode = error.statusCode; //|| error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+    const statusCode = error.statusCode;
     const message = error.message || httpStatus[statusCode];
     error = new ApiError(statusCode, message, false, err.stack);
   }
   next(error);
 };
 
-// eslint-disable-next-line no-unused-vars
+// 에러 핸들링
 const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
-  if (config.env === 'production' && !err.isOperational) {
-    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
-  }
 
   res.locals.errorMessage = err.message;
 
   const response = {
     code: statusCode,
     message,
-    ...(config.env === 'development' && { stack: err.stack }),
   };
-
-  if (config.env === 'development') {
-    logger.error(err);
-  }
 
   res.status(statusCode).send(response);
 };
