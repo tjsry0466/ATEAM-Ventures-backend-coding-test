7119aa2518194a787cdc18a4197fa57ce1f86a33
jeong seonkyo <seonkyo0466@gmail.com>
2021-12-10 20:57:23 +0900

    validation 적용

--- src/routes/v1/postCodes.route.js
+++ src/routes/v1/postCodes.route.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const validate = require('../../middlewares/validate');
 const { postCodesController } = require('../../controllers/');
+const { postCodesValidation } = require('../../validations/');
 
-router.route('/stores').get(postCodesController.getStores);
-router.route('/stores/:storeName').get(postCodesController.getStore);
+router
+  .route('/stores')
+  .get(validate(postCodesValidation.getStores), postCodesController.getStores);
+
+router
+  .route('/stores/:storeName')
+  .get(validate(postCodesValidation.getStore), postCodesController.getStore);
 
 router
   .route('/post-codes/:postCode/radius-search')
-  .get(postCodesController.getRadiusStoresByPostCode);
+  .get(
+    validate(postCodesValidation.getRadiusStoresByPostCode),
+    postCodesController.getRadiusStoresByPostCode
+  );
 
 module.exports = router;
--- src/validations/postCodes.validation.js
+++ src/validations/postCodes.validation.js
@@ -1,3 +1,30 @@
 const Joi = require('joi');
 
-module.exports = {};
+const getStores = {
+  body: Joi.object().keys({}),
+};
+
+const getRadiusStoresByPostCode = {
+  params: Joi.object().keys({
+    postCode: Joi.string()
+      .regex(/^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$/)
+      .required(),
+  }),
+  query: Joi.object().keys({
+    limit: Joi.number().min(1).max(100),
+    radius: Joi.number().min(1).max(2000),
+    widesearch: Joi.boolean(),
+  }),
+};
+
+const getStore = {
+  params: Joi.object().keys({
+    storeName: Joi.string().required(),
+  }),
+};
+
+module.exports = {
+  getStores,
+  getRadiusStoresByPostCode,
+  getStore,
+};
